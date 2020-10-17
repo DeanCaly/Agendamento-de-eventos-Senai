b5b741e3a576a681d7a2e565c25cc34666b51bbd
AmandaGundes <amandagundes@outlook.com>
2020-10-17 12:58:05 -0300

    Inclusão dos métodos de cadastro, edição e exclusão de eventos

--- app/src/main/java/br/senai/cadastrodeeventossenai/CadastrarEvento.java
+++ app/src/main/java/br/senai/cadastrodeeventossenai/CadastrarEvento.java
@@ -2,41 +2,97 @@ package br.senai.cadastrodeeventossenai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import br.senai.cadastrodeeventossenai.modelo.Evento;
 
 public class CadastrarEvento extends AppCompatActivity {
+
+    private final int RESULT_CODE_NOVO_EVENTO = 10;
+    private final int RESULT_CODE_EVENTO_EDITADO = 11;
+    private final int RESULT_CODE_EVENTO_EXCLUIDO = 12;
+
+    private boolean edicao = false;
+    private int id = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_evento);
-        setTitle("Agendamentos");
+        setTitle("Agendamento de Eventos");
+        carregarEvento();
+
+        final EditText editTextData = (EditText) findViewById(R.id.editText_data);
+        editTextData.addTextChangedListener(Mask.insert("##/##/####", editTextData));
     }
 
+    private void carregarEvento() {
+        Intent intent = getIntent();
+        if (intent != null && intent.getExtras() != null && intent.getExtras().get("eventoEdicao") != null) {
+            Evento evento = (Evento) intent.getExtras().get("eventoEdicao");
+            EditText editTextNome = findViewById(R.id.editText_nome);
+            EditText editTextData = findViewById(R.id.editText_data);
+            EditText editTextLocal = findViewById(R.id.editText_local);
+            editTextNome.setText(evento.getNome());
+            editTextData.setText(String.valueOf(evento.getData()));
+            editTextLocal.setText(evento.getLocal());
+            edicao = true;
+            id = evento.getId();
+        }
+    }
 
-    public void OnclickVoltar(View v) {
+    public void onClickVoltar(View v) {
         finish();
     }
 
+    public void oncClickSalvar(View v) {
+        EditText editTextNome = findViewById(R.id.editText_nome);
+        EditText editTextData = findViewById(R.id.editText_data);
+        EditText editTextLocal = findViewById(R.id.editText_local);
 
+        String nome = editTextNome.getText().toString();
+        String local = editTextLocal.getText().toString();
 
-
-    public void OncClickSalvar(View v) {
-        EditText editTextNome = findViewById(R.id.EditText_nomeEvento);
-        EditText editTextdata = findViewById(R.id.editText_dataEvento);
-        EditText editTextLocal = findViewById(R.id.editText_localEvento);
-
-        String nomeEvento = editTextNome.getText().toString();
-        String dataEvento = editTextdata.getText().toString();
-        String localEvento = editTextLocal.getText().toString();
-
+        if (edicao) {
+            LocalDate data = LocalDate.parse(editTextData.getText().toString());
+            Evento evento = new Evento(id, nome, data, local);
+            Intent intent = new Intent();
+            intent.putExtra("eventoEditado", evento);
+            setResult(RESULT_CODE_EVENTO_EDITADO, intent);
+        } else {
+            LocalDate data = LocalDate.parse(editTextData.getText().toString(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            Evento evento = new Evento(id, nome, data, local);
+            Intent intent = new Intent();
+            intent.putExtra("novoEvento", evento);
+            setResult(RESULT_CODE_NOVO_EVENTO, intent);
+        }
         Toast.makeText(CadastrarEvento.this, "Agendamento feito com sucesso!", Toast.LENGTH_SHORT).show();
         finish();
+    }
 
+    public void onClickExcluir(View v) {
+        EditText editTextNome = findViewById(R.id.editText_nome);
+        EditText editTextData = findViewById(R.id.editText_data);
+        EditText editTextLocal = findViewById(R.id.editText_local);
+
+        String nome = editTextNome.getText().toString();
+        LocalDate data = LocalDate.parse(editTextData.getText().toString());
+        String local = editTextLocal.getText().toString();
+
+        Evento evento = new Evento(id, nome, data, local);
+        Intent intent = new Intent();
+
+        intent.putExtra("eventoExcluido", evento);
+        setResult(RESULT_CODE_EVENTO_EXCLUIDO, intent);
+
+        Toast.makeText(CadastrarEvento.this, "Agendamento excluído com sucesso!", Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
--- app/src/main/java/br/senai/cadastrodeeventossenai/modelo/Evento.java
+++ app/src/main/java/br/senai/cadastrodeeventossenai/modelo/Evento.java
@@ -1,17 +1,32 @@
 package br.senai.cadastrodeeventossenai.modelo;
 
-import java.time.LocalDate;
+import androidx.annotation.NonNull;
 
-public class Evento {
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Evento implements Serializable {
+
+    private int id;
     private String nome;
     private LocalDate data;
     private String local;
 
 
-    public Evento(String nome, LocalDate data, String local) {
+    public Evento(int id, String nome, LocalDate data, String local) {
+        this.id = id;
         this.nome = nome;
         this.data = data;
         this.local = local;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getNome() {
@@ -26,8 +41,10 @@ public class Evento {
         return data;
     }
 
-    public void setData(LocalDate data) {
-        this.data = data;
+    public void setData(String data) {
+        String format = "dd/MM/yyyy";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        this.data = LocalDate.parse(data, formatter);
     }
 
     public String getLocal() {
@@ -38,6 +55,7 @@ public class Evento {
         this.data = data;
     }
 
+    @NonNull
     @Override
     public String toString() {
         return nome + "\n" +
